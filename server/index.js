26eef92ce7fe8e07b86b9cff00e1d2e8ba22eb8e
alopvaz <alopvaz014@iesalmudeyne.es>
2024-02-01 17:21:51 +0100

    modificacion exportar en index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-import db from './conexion.js';
+const db = require('./conexion.js');
 
-import express from 'express';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 3001;
