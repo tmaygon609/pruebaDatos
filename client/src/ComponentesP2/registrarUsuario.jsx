86408e0ff936d8860c9bb5af395f491ab8436a84
tmaygon609@iesalmudeyne.es <tmaygon609@iesalmudeyne.es>
2024-02-01 16:40:57 +0100

    Subida componente

--- client/src/ComponentesP2/registrarUsuario.jsx
+++ client/src/ComponentesP2/registrarUsuario.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const RegistrarUsuarioForm = () => {
   // Estado para almacenar los datos del formulario
@@ -29,10 +30,19 @@ const RegistrarUsuarioForm = () => {
   };
 
   // Función que se ejecuta cuando se envía el formulario
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Luego meteremos los datos en la bbdd 
-    console.log('Formulario enviado:', formData);
+
+    try {
+      // Realizar una solicitud POST al servidor
+      const response = await axios.post('http://localhost:3001/usuario', formData);
+
+      // Manejar la respuesta del servidor según tus necesidades
+      console.log('Respuesta del servidor:', response.data);
+    } catch (error) {
+      // Manejar errores de la solicitud al servidor
+      console.error('Error al enviar datos al servidor:', error.message);
+    }
   };
 
   // Función que se ejecuta cuando se selecciona un campo
