f69c6161c611fbe087a372e95910d26d21448752
tmaygon609@iesalmudeyne.es <tmaygon609@iesalmudeyne.es>
2024-02-01 19:48:06 +0100

    Rectificacion

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@
 const express = require('express');
 const cors = require('cors');
 //Nuestras rutas
-const routeActualizarUsuario = require('./routes/routeActualizarUsuario.js');
-const routeAñadirUsuario = require('./routes/routeAñadirUsuario.js');
-const routeEliminarUsuario = require('./routes/routeEliminarUsuario.js');
-const routeLoginUsuario = require('./routes/routeLoginUsuario.js');
-const routeRegistroNotas = require('./routes/routeRegistroNotas.js');
+// const routeActualizarUsuario = require('./routes/routeActualizarUsuario.js');
+// const routeAñadirUsuario = require('./routes/routeAñadirUsuario.js');
+// const routeEliminarUsuario = require('./routes/routeEliminarUsuario.js');
+// const routeLoginUsuario = require('./routes/routeLoginUsuario.js');
+// const routeRegistroNotas = require('./routes/routeRegistroNotas.js');
 //Ya implementado
 const routeRegistroUsuario = require('./routes/routeRegistroUsuario.js');
 
@@ -16,11 +16,11 @@ const port = 3001;
 app.use(express.json());
 app.use(cors());
 //Llamada a nuestras rutas
-app.use('/', routeActualizarUsuario);
-app.use('/', routeAñadirUsuario);
-app.use('/', routeEliminarUsuario);
-app.use('/', routeLoginUsuario);
-app.use('/', routeRegistroNotas);
+// app.use('/', routeActualizarUsuario);
+// app.use('/', routeAñadirUsuario);
+// app.use('/', routeEliminarUsuario);
+// app.use('/', routeLoginUsuario);
+// app.use('/', routeRegistroNotas);
 //Ya implementado
 app.use('/', routeRegistroUsuario);
 
