dc0676ca544eb53f52b92e35934cd09997e4418a
tmaygon609@iesalmudeyne.es <tmaygon609@iesalmudeyne.es>
2024-02-01 16:11:08 +0100

    Primer commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import RegistrarUsuarioForm from './components/registrarUsuario';
+
+function App() {
+  return (
+    <div className="App">
+      <RegistrarUsuarioForm></RegistrarUsuarioForm>
+    </div>
+  );
+}
+
+export default App;
