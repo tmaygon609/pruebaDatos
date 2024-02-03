46146bcea3261da76a80167247bd5bff4dc3e2c5
tmaygon609@iesalmudeyne.es <tmaygon609@iesalmudeyne.es>
2024-02-03 21:29:17 +0100

    Terminado el bbdd

--- server/routes/routeRegistroUsuario.js
+++ server/routes/routeRegistroUsuario.js
@@ -20,18 +20,37 @@ app.get('/usuarios', (req, res) => {
 app.post('/usuario', (req, res) => {
     const { nombre, apellidos, departamento, usuario, password } = req.body;
 
-    const sql = 'INSERT INTO usuarios (nombre, apellidos, departamento, usuario, password) VALUES (?, ?, ?, ?, ?)'
-    const values = [nombre, apellidos, departamento, usuario, password];
+    // Comprobar si el usuario ya existe
+    const checkUsuarioSql = 'SELECT * FROM usuarios WHERE usuario = ?';
 
-    db.query(sql, values, (err) => {
-        if (err) {
-            console.log('Error al insertar datos: ' + err.message);
+    db.query(checkUsuarioSql, usuario, (checkErr, checkResult) => {
+
+        if (checkErr) {
+            console.log('Error en la consulta SQL: ' + checkErr.message);
             res.status(500).json({ error: 'Error interno del servidor' });
         } else {
-            console.log('Datos insertados');
-            res.json({ mensaje: 'Usuario registrado correctamente' });
+
+            if (checkResult.lenght > 0) {
+                // El usuario ya existe
+                res.status(409).json({ error: 'El usuario ya existe' });
+            } else {
+                // El usuario no existe.
+                const insertarUsuarioSql = 'INSERT INTO usuarios (nombre, apellidos, departamento, usuario, password) VALUES (?, ?, ?, ?, ?)'
+                const values = [nombre, apellidos, departamento, usuario, password];
+
+                db.query(insertarUsuarioSql, values, (insertErr) => {
+                    if (err) {
+                        console.log('Error al insertar datos: ' + insertErr.message);
+                        res.status(500).json({ error: 'Error interno del servidor' });
+                    } else {
+                        console.log('Datos insertados');
+                        res.json({ mensaje: 'Usuario registrado correctamente' });
+                    }
+                });
+            }
         }
     });
+
 });
 
 module.exports = app;
