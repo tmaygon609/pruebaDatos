00bd9527a029bfb96510756cb8876a2f4bbb17f3
alopvaz <alopvaz014@iesalmudeyne.es>
2024-02-05 19:46:14 +0100

    cambio ali actualizacion

--- client/src/ComponentesP2/Actualizacion.jsx
+++ client/src/ComponentesP2/Actualizacion.jsx
@@ -16,6 +16,7 @@ function Actualizacion() {
         console.error('Error al obtener los datos de los alumnos:', error);
         setErrors({ message: 'Error al obtener los datos de los alumnos' });
       });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []); // El array vacío como segundo argumento significa que este efecto se ejecutará solo una vez, al montar el componente
 
   const handleEdit = (id) => {
